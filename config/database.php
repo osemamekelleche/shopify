3b38df1495902d07105c6310c52ee905edeb4c6a
Osema MEKELLECHE <osema.meke@gmail.com>
2026-04-27 15:19:33 +0100

    Change SQLite database path to private_data

--- config/database.php
+++ config/database.php
@@ -34,7 +34,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => storage_path('app/private/database.sqlite'),
+            'database' => base_path('private_data/database.sqlite'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,

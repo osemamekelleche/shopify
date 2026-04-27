bd36057d1a73013f43e57c0f0654ed8f61bb9ab1
Osema MEKELLECHE <osema.meke@gmail.com>
2026-04-27 15:35:16 +0100

    Fix path for Shopify stores JSON file

--- config/shopify.php
+++ config/shopify.php
@@ -2,7 +2,7 @@
 
 return [
     'stores' => json_decode(json:
-        file_exists($path = base_path('../private_data/shopify_stores.json')) ?
+        file_exists($path = storage_path('app/private/shopify_stores.json')) ?
             file_get_contents($path) :
             '',
         associative: true
